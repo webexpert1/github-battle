8b8707a92ecda36c61e4c158eba4ed2c7d274eed
github_webexpert1 <richokoye1@gmail.com>
2019-06-01 17:46:30 +0100

    add result component

--- src/components/Battle.js
+++ src/components/Battle.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { FaUserFriends, FaFighterJet, FaTrophy} from 'react-icons/fa';
+import { FaUserFriends, FaFighterJet, FaTrophy, FaTimesCircle} from 'react-icons/fa';
 import PropTypes from 'prop-types';
+import Results from './Results';
 
 function Instructions() {
     return ( 
@@ -67,23 +68,58 @@ PlayerInput.propTypes = {
     label: PropTypes.string.isRequired
 }
 
+function PlayerPreview({ username, onReset, label}) {
+   return (
+       <div className="column player">
+           <h3 className="player-label">{label}</h3>
+           <div className="row bg-light">
+               <div className="player-info">
+                   <img className="avatar-small" 
+                        src={`https://github.com/${username}.png?size=200`}
+                        alt={`Avatar for ${username}`} />
+                    <a href={`https://github.com/${username}`}
+                        className="link">{username}</a>
+               </div>
+               <button className="btn-clear flex-center" onClick={onReset}>
+                   <FaTimesCircle color="rgb(194, 57, 42)" size={26} />
+               </button>
+           </div>
+       </div>
+   ) 
+}
+PlayerPreview.propTypes = {
+    username: PropTypes.string.isRequired,
+    onReset: PropTypes.func.isRequired,
+    label: PropTypes.string.isRequired
+}
 export default class Battle extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
             playerOne: null,
-            playerTwo: null
+            playerTwo: null,
+            battle: false
         };
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleReset = this.handleReset.bind(this);
     }
     handleSubmit(id, player) {
         this.setState({
             [id]: player
         })
     }
+    handleReset(id) {
+        this.setState({
+            [id]: null
+        })
+    }
     render() { 
-        const { playerOne, playerTwo } = this.state;
+        const { playerOne, playerTwo, battle } = this.state;
+
+        if(battle === true) {
+            return <Results playerOne={playerOne} playerTwo={playerTwo} />
+        }
         return ( 
             <React.Fragment>
                 <Instructions />
@@ -91,14 +127,21 @@ export default class Battle extends Component {
                 <div className="players-container">
                     <h1 className="center-text header-lg">Players</h1>
                     <div className="row space-around">
-                        {playerOne === null && (
-                             <PlayerInput label="Player One" onSubmit={(player) => this.handleSubmit('playerOne', player) } />
-                        )}
+                        {playerOne === null 
+                        ? <PlayerInput label="Player One" onSubmit={(player) => this.handleSubmit('playerOne', player) } />
+                        : <PlayerPreview username={playerOne} label='Player One' onReset={() => this.handleReset('playerOne')} />
+                        }
                     
-                        {playerTwo === null && (
-                             <PlayerInput label="Player Two" onSubmit={(player) => this.handleSubmit('playerTwo', player) } />
-                        )}
+                        {playerTwo === null
+                        ? <PlayerInput label="Player Two" onSubmit={(player) => this.handleSubmit('playerTwo', player) } />
+                        : <PlayerPreview username={playerTwo} label="Player Two" onReset={() => this.handleReset('playerTwo')} /> 
+                        }
                     </div>
+
+                    {playerOne && playerTwo && (
+                        <button className="btn dark-btn btn-space"
+                                onClick={() => this.setState({battle: true})}>Battle</button>
+                    )}
                 </div> 
                
             </React.Fragment>
